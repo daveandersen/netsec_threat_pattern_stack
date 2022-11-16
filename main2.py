76d59436557b7966bb71e7aa49b7909089e6d0dd
Dave Andersen <daveytheboo@gmail.com>
2022-11-16 20:39:05 +0700

    restore deleted functions in main2

--- main2.py
+++ main2.py
@@ -24,6 +24,8 @@ with driver.session() as session:
         session.write_transaction(create_target_node, request['description'])
         session.write_transaction(create_target_port, fields['target_port'])
         session.write_transaction(create_source_address_and_country_relationship, fields['source_address'], attacker_location['country_code'])
+        session.write_transaction(create_source_address_and_registered_country_relationship, fields['source_address'], attacker_location['country_code'])
+        session.write_transaction(create_source_address_and_target_port_relationship, fields['source_address'], fields['target_port'])
         session.write_transaction(create_attack_relationship, fields['source_address'], request['description'])
 
     placeholder = session.read_transaction(get_country)
