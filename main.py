bf8ea6dbe6de39a8b6f0976e5ad782a85a7bf32a
Dave Andersen <daveytheboo@gmail.com>
2022-12-05 09:30:15 +0700

    changed main2 to main

--- main.py
+++ main.py
@@ -1,63 +1,114 @@
 from pymongo import MongoClient
-from neomodel import StructuredNode, StringProperty, RelationshipTo, RelationshipFrom, config
-
-# Connecting to MongoDB
-cluster = MongoClient("mongodb://localhost:27017")
-db = cluster["Neo4j_DB"]
-collection = db['NBA']
-
-# MongoDB Connecting to Neo4j
-config.DATABASE_URL = 'bolt://neo4j:sgunetsec@localhost:7687/neo4j'
-
-class Player(StructuredNode):
-    name = StringProperty(unique_index=True)
-    coach = RelationshipFrom('Coach', 'COACH')
-
-class Coach(StructuredNode):
-    name = StringProperty(unique_index=True)
-    player = RelationshipTo('Player', 'PLAYER')
-
-# cursor = collection.find({})
-# for document in cursor:
-#     print(document['n'])
-#     id_name = document['n']['properties']['name']
-#     if(document['n']['labels'][0] == 'PLAYER'):
-#         Player(name=id_name).save()
-#     if(document['n']['labels'][0] == 'COACH'):
-#         Coach(name=id_name).save()
-
-lebronJames = Player.nodes.first(name="LeBron James")
-frankVogel = Coach.nodes.first(name="Frank Vogel")
-
-# We can save relationship to the variable so we can send it to ES
-# lebronFrank = lebronJames.coach.connect(frankVogel)
-
+from neo4j import GraphDatabase
+from Node import *
+from NodeRelationship import *
+from GetNodes import *
+from CypherQuery import *
 from elasticsearch7 import Elasticsearch
+from graphdatascience import GraphDataScience
 
-# Neo4j Connecting to Elasticsearch
+cluster = MongoClient("mongodb://localhost:27017")
+db = cluster["neo4j"]
+collection = db['ssh_commands']
+
+driver = GraphDatabase.driver(uri = "bolt://localhost:7687", auth=("neo4j", "sgunetsec"))
 es = Elasticsearch(HOST="http://localhost", PORT=9200)
 
-#Insert data to Elasticsearch
-i = 0
-for player in Player.nodes:
-    objectPlayer = {"name": player.name}
-    es.index(index="player", doc_type="players", id=i, document=objectPlayer)
-    i+=1
+with driver.session() as session:
+    cursor = collection.find({})
+    for document in cursor:
+        fields = document['fields']
+        request = document['request_']
+        country = document['honeypot_country']
+        attacker_location = document['geo_location']
+        threats = document['threat']
+        # print(threats)
+        # print(fields['source_address'])
+        if(request == {}):
+            request['description'] = "SSH Honeypot Cowrie"
+        
+        behavior = []
+        i = 0
+        for threat in threats:
 
+            if(threat.get('matching_syntax') is None):
+                behavior.append(threat['unknown_syntax'])
+                session.execute_write(create_command, threat['unknown_syntax'])
+                session.execute_write(create_use_command_relationship, fields['source_address'], threat['unknown_syntax'])
+                es.index(index="unknown_command", doc_type="unknown_commands", id = i, document=threat)
+                i+=1
 
-i = 0
-for coach in Coach.nodes:
-    objectCoach = {"name": coach.name}
-    es.index(index="coach", doc_type="coaches", id=i, body=objectCoach)
-    i+=1
-
-# es.index(index="coaches_relationship", doc_type="coaches_relationships", id = 0, body=)
-
-#Getting data from Elasticsearch
-# player1 = es.get(index="player", doc_type="players", id= 0)
-# coach1 = es.get(index="coach", doc_type="coaches", id= 0)
-
-# print(player1)
-# print(coach1)
+            else:
+                behavior.append(threat['matching_syntax'])
+                session.execute_write(create_command, threat['matching_syntax'])
+                session.execute_write(create_use_command_relationship, fields['source_address'], threat['matching_syntax'])
+                #Start threat pattern behavior categorization
+                session.execute_write(create_threat_category, threat['threat_category'])
+                session.execute_write(create_threat_purpose, threat['threat_purpose'])
+                session.execute_write(create_threat_phase, threat['threat_phase'])
+                session.execute_write(create_threat_categorized_as_relationship, threat['matching_syntax'], threat['threat_category'])
+                session.execute_write(create_threat_for_relationship, threat['threat_category'], threat['threat_purpose'])
+                session.execute_write(create_threat_in_phase_relationship, threat['threat_purpose'], threat['threat_phase'])
+                es.index(index="threat", doc_type="threats", id = i, document=threat)
+                print(threat)
+                print("\n")
+                i+=1     
+            
+        print(hash(tuple(behavior)))
+        delete_behavior(session)
+        session.execute_write(create_behavior, hash(tuple(behavior)))
+        session.execute_write(create_behavior_relationship, fields['source_address'], hash(tuple(behavior)))
+                
+        session.execute_write(create_source_address, fields['source_address'])
+        session.execute_write(create_country_code, attacker_location['country_code'], attacker_location['country'])
+        session.execute_write(create_target_node, request['description'])
+        session.execute_write(create_target_port, fields['target_port'])
+        session.execute_write(create_source_address_and_country_relationship, fields['source_address'], attacker_location['country_code'])
+        session.execute_write(create_source_address_and_registered_country_relationship, fields['source_address'], attacker_location['country_code'])
+        session.execute_write(create_source_address_and_target_port_relationship, fields['source_address'], fields['target_port'])
+        session.execute_write(create_attack_relationship, fields['source_address'], request['description'])
     
+    countries = get_country(session)
+    categories = get_threat_category(session)
+    # overallRelationship = get_overall_relationship(session)
+    # print(overallRelationship)
 
+    i = 0
+    for country in countries.data():
+        # print(country)
+        es.index(index="country", doc_type="countries", id = i,document=country)
+        i+=1
+
+    attack_relationships = get_attacks_relationship(session)
+    i = 0
+    for attack in attack_relationships.data():
+        attack_temp_object = {
+            "relationship": attack['p'][1],
+            "properties": {
+              "source": attack['p'][0]['source_address'],
+              "target": attack['p'][2]['target']
+            }
+        }
+        # print(attack_temp_object)
+        es.index(index="attack", doc_type="attacks", id = i,document=attack_temp_object)
+        i+=1
+
+    create_ip_commands_graph(session)
+    ip_commands_similarity = create_ip_commands_graph_similarity(session)
+    # print(ip_commands_similarity.data())
+
+    i = 0
+    for similarity1 in ip_commands_similarity.data():
+        es.index(index="ip_commands_similarity", doc_type="ip_commands_similarities", id = i, document=similarity1)
+        i+=1
+    
+    create_ip_behave_graph(session)    
+    ip_behave_similarity = create_ip_behave_graph_similarity(session)
+    i = 0
+    for similarity2 in ip_behave_similarity.data():
+        es.index(index="ip_behave_similarity", doc_type="ip_behave_similarities", id = i, document=similarity2)
+        i+=1
+driver.close()
+
+# get all data from neo4j and then export to elasticsearch
+        
